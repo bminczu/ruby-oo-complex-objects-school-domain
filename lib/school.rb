73c9774facc58fd5c1377cec9d3867a5f546c487
bminczu <bminczu@gmail.com>
2020-10-27 20:42:59 -0500

    commit tree c849b1426b50eae9a948640d0870600714cac7b1

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
 # code here!
+require 'pry'
+class School
+    def initialize(name)
+        @name = name
+        @hash = {}
+    end
+
+    def roster
+        @hash={}
+    end
+    
+    def add_student(name, grade)
+    end
+end
